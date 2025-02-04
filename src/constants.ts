4f678299ab5f9f39b8e29e239deff717a7a8c0ca
Adalbert Makarovych <amakarovych0ua@singlestore.com>
2025-02-04 17:17:21 +0200

    Updated SDK

--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,19 @@
-import { CapabilitiesResponse, ScalarType } from "@hasura/ndc-sdk-typescript";
+import { Capabilities, ScalarType } from "@hasura/ndc-sdk-typescript";
 
-export const CAPABILITIES_RESPONSE: CapabilitiesResponse = {
-    version: "0.1.2",
-    capabilities: {
-        query: {
-            aggregates: {},
-            variables: {},
-            explain: {}
-        },
-        mutation: {
-            transactional: {},
-            explain: {}
-        },
-        relationships: {
-            order_by_aggregate: {},
-            relation_comparisons: {}
-        }
+export const CAPABILITIES_RESPONSE: Capabilities = {
+    query: {
+        aggregates: {},
+        variables: {},
+        explain: {}
     },
+    mutation: {
+        transactional: {},
+        explain: {}
+    },
+    relationships: {
+        order_by_aggregate: {},
+        relation_comparisons: {}
+    }
 };
 
 export enum DataTypeClass {
